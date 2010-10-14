a0e139b08b213f96175b860b0cd48000c68ee437
Leo Antunes <leo@costela.net>
2010-10-14 03:25:27 +0200

    Imported Upstream version 2.10

---
+++ libtransmission/version.h
@@ -0,0 +1,8 @@
+#define PEERID_PREFIX             "-TR2100-"
+#define USERAGENT_PREFIX          "2.10"
+#define SVN_REVISION              "11294"
+#define SVN_REVISION_NUM          11294
+#define SHORT_VERSION_STRING      "2.10"
+#define LONG_VERSION_STRING       "2.10 (11294)"
+#define VERSION_STRING_INFOPLIST  2.10
+#define TR_STABLE_RELEASE         1
