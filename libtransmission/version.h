2036bb4a606afa0dcbbdd047981c00d97c03b119
Andreas Noteng <andreas@noteng.no>
2011-05-19 21:35:42 +0200

    Imported Upstream version 2.30

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -1,10 +1,10 @@
-#define PEERID_PREFIX             "-TR2220-"
-#define USERAGENT_PREFIX          "2.22"
-#define SVN_REVISION              "12099"
-#define SVN_REVISION_NUM          12099
-#define SHORT_VERSION_STRING      "2.22"
-#define LONG_VERSION_STRING       "2.22 (12099)"
-#define VERSION_STRING_INFOPLIST  2.22
+#define PEERID_PREFIX             "-TR2300-"
+#define USERAGENT_PREFIX          "2.30"
+#define SVN_REVISION              "12439"
+#define SVN_REVISION_NUM          12439
+#define SHORT_VERSION_STRING      "2.30"
+#define LONG_VERSION_STRING       "2.30 (12439)"
+#define VERSION_STRING_INFOPLIST  2.30
 #define MAJOR_VERSION             2
-#define MINOR_VERSION             22
+#define MINOR_VERSION             30
 #define TR_STABLE_RELEASE         1
